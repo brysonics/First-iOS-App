ff5ae40aa9cc4ed5e6b10c5f28afcda3b74e58f5
bryson <brysonkaranja@gmail.com>
2023-05-15 19:45:39 +0300

    Initial Commit

---
+++ L3 Demo/L3_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L3_DemoApp.swift
+//  L3 Demo
+//
+//  Created by Bryson Karanja on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct L3_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
