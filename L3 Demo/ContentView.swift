a876911930152c93af042d2b38f84505a564e4b2
bryson <brysonkaranja@gmail.com>
2023-05-16 16:26:54 +0300

    v1

--- L3 Demo/ContentView.swift
+++ L3 Demo/ContentView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.gray)
+                .ignoresSafeArea()
+            
+            
+            VStack {
+                Image("elon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding(5)
+                HStack {
+                    Spacer()
+                    Text("elon musk")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                    Spacer()
+                    
+                    }
+                VStack{
+                    Spacer()
+                        .frame(width: 0.0, height: 2.0)
+                    Image(systemName: "bird.fill")
+                        .foregroundColor(Color.blue)
+                    Text("CEO of twitter")
+                        .font(.footnote)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.black)
+                    
+                    
+                }
+                
+                
+                
+                    
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.cyan)
+                .cornerRadius(15)
+                .shadow(radius: 15))
+            .padding()
         }
-        .padding()
+        
+        
+
+        
+        
     }
 }
 
@@ -24,3 +66,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
